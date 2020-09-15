251e1ded9f4e81dac172910c04b6559ccffedf07
Marco Ghibaudi <marco.ghibaudi@riverlane.com>
2020-09-15 16:03:31 +0100

    DFPROTO-208: adding emulator mode to the bootloader/runtime

--- artiq/firmware/bootloader/main.rs
+++ artiq/firmware/bootloader/main.rs
@@ -19,6 +19,7 @@ use board_misoc::slave_fpga;
 use board_misoc::{clock, ethmac, net_settings};
 use board_misoc::uart_console::Console;
 
+#[cfg(not(has_emulator))]
 fn check_integrity() -> bool {
     extern {
         static _begin: u8;
@@ -34,6 +35,12 @@ fn check_integrity() -> bool {
     }
 }
 
+#[cfg(has_emulator)]
+fn check_integrity() -> bool {
+    return true; 
+}
+
+#[cfg(not(has_emulator))] 
 fn memory_test(total: &mut usize, wrong: &mut usize) -> bool {
     const MEMORY: *mut u32 = board_mem::MAIN_RAM_BASE as *mut u32;
 
@@ -83,6 +90,11 @@ fn memory_test(total: &mut usize, wrong: &mut usize) -> bool {
             for i in (0..0x10000) { MEMORY[prng16(&mut seed)] = i });
     }
     *wrong == 0
+}
+
+#[cfg(has_emulator)]
+fn memory_test(total: &mut usize, wrong: &mut usize) -> bool {
+    return true;
 }
 
 fn startup() -> bool {
@@ -154,6 +166,7 @@ fn load_slave_fpga() {
     println!("  ...done");
 }
 
+#[cfg(not(has_emulator))]
 fn flash_boot() {
     const FIRMWARE: *mut u8 = board_mem::FLASH_BOOT_ADDRESS as *mut u8;
     const MAIN_RAM: *mut u8 = board_mem::MAIN_RAM_BASE as *mut u8;
@@ -191,6 +204,13 @@ fn flash_boot() {
         println!("Firmware CRC failed in flash (actual {:08x}, expected {:08x})",
                  actual_crc_flash, expected_crc);
     }
+}
+
+#[cfg(has_emulator)]
+fn flash_boot(){
+    println!("Flash boot - RAM has been preloaded with the firmware. We skip all the initialization and jump into main");
+    const MAIN_RAM: *mut u8 = (board_mem::MAIN_RAM_BASE + 0x1034) as *mut u8;
+    unsafe { boot::jump(MAIN_RAM as usize) }
 }
 
 #[cfg(has_ethmac)]
